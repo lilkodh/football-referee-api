d7b0ed656a3c2281aba6e3972edd74f166a839ae
lilkodh <khaliddrihemdev@gmail.com>
2026-07-05 18:33:31 +0100

    feat : fix name of an file

--- src/routes/Assignments.Route.js
+++ src/routes/Assignments.Route.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const AssignmentController = require("../controllers/Asignment.Controller");
+const AssignmentController = require("../controllers/Assignment.Controller");
 const {validateAssignment} = require("../middlewares/validate.middleware")
 router.get("/", AssignmentController.getAll);
 router.post("/",validateAssignment, AssignmentController.create);
