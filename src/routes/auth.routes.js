9f071f5edfda1921d53f78532f9cf49c5eaaaccc
lilkodh <khaliddrihemdev@gmail.com>
2026-07-11 23:19:48 +0100

    feat(validation): integrate Zod validation into auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,18 +3,21 @@ const router = express.Router();
 const AuthController = require("../controllers/Auth.controller");
 const authenticate = require("../middlewares/authenticate.middleware");
 const authorize = require("../middlewares/authorize.middleware");
+const zodValidate = require("../middlewares/zodValidate.middleware");
+const {
+  userRegistrationSchema,
+  userLoginSchema,
+} = require("../schemas/auth.schema");
+
 router.post(
-  "/register",
+  "/register", zodValidate(userRegistrationSchema),
   authenticate,
   authorize("admin"),
   AuthController.register,
 );
 
-router.post("/login", AuthController.login);
+router.post("/login", zodValidate(userLoginSchema),AuthController.login);
 
 router.get("/me", authenticate, AuthController.me);
 
-
-
-
-module.exports = router ;
+module.exports = router;
