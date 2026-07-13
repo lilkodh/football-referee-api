e8d96177e36f70db4d61479b2d17537613499e4b
lilkodh <khaliddrihemdev@gmail.com>
2026-07-13 09:27:23 +0100

    feat( add email format)

--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -1,13 +1,13 @@
 const { z, } = require("zod");
 const userRegistrationSchema = z.object({
   name: z.string().min(8),
-  email: z.string(),
+  email: z.string().email(),
   password: z.string().min(6),
   role: z.enum(["admin", "commissioner", "referee", "consultation"]),
   refereeId: z.coerce.number().int().positive().optional(),
 });
 const userLoginSchema = z.object({
-  email: z.string().min(8),
+  email: z.string().email().min(8),
   password: z.string().min(6),
 });
 const changePasswordSchema =z.object({
